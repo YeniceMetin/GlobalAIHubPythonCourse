14733102d02635d81b0f39d375dc34f17e4f54ba
Metin YENİCE <82218126+YeniceMetin@users.noreply.github.com>
2021-04-09 16:48:38 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,9 +1,9 @@
 #Explain your work
 #birinci yöntem sabit bir dizi üzerinde bölme ve yazdırma işlemi
 dz=list(range(10))
-print("dizinin orjinal hali")
+print("DİZİMİZİN İLK HALİ")
 print (dz)
 dz1=dz[:5]
 dz2=dz[5:]
-print("\nbölümüş diziler")
+print("\nBÖLÜNEN DİZİLER")
 print(dz2,dz1)
