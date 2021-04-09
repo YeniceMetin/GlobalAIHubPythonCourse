b099709ba685059c4b80a2b4fb82d38838aeaf8e
Metin YENİCE <82218126+YeniceMetin@users.noreply.github.com>
2021-04-09 17:19:46 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,18 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+a=0
+kullanıcıadı=input("KULLANICI ADINIZI GİRİNİZ : ")
+if(kullanıcıadı=="my"):
+    while True:
+        pword=input("ŞİFRENİZİ GİRİNİZ : ")
+        if(pword=="123456"):
+            print("SİSTEME GİRDİNİZ.")
+            break
+        else:
+            print("ŞİFRENİZİ YANLIŞ GİRDİNİZ")
+            a+=1
+            if(a==3):
+                print("3 KERE YANLIŞ GİRİŞ YAPTINIZ.\nBİR SÜRE SONRA TEKRAR DENEYİNİZ")
+                break
+            continue
+else:
+    print("SİSTEMDE KAYDINIZ BULUNMAMAKTADIR")
+    print("SİSTEME KAYIT YAPTIRABİLİRSİNİZ")
